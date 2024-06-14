e146eb606dd71a069b7fe2d2cc040dd8760336f9
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-14 11:32:54 -0300

    operadores aritméticos e lógicos

---
+++ fundamentos/operadores.dart
@@ -0,0 +1,23 @@
+main() {
+  // Operadores Aritméticos (operadores binários)
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % 2);
+  print(a + (b * a) - (b / a));
+
+  // Operadores Lógicos
+  bool produtoFragil = true;
+  bool produtoCaro = false;
+
+  print(produtoFragil && produtoCaro); // E
+  print(produtoFragil || produtoCaro); // OU
+  print(produtoFragil ^ produtoCaro); // OU exclusivo
+  print(!produtoFragil); // Negação
+  print(!!produtoFragil); // Negação
+}
