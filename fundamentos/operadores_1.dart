c8ec7822c6fcac83fa0d7610b7454e4597d71801
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-14 15:16:54 -0300

    operadores

--- fundamentos/operadores_1.dart
+++ fundamentos/operadores_1.dart
@@ -16,6 +16,7 @@ main() {
   bool produtoCaro = false;
 
   print(produtoFragil && produtoCaro); // E
+  // ignore: dead_code
   print(produtoFragil || produtoCaro); // OU
   print(produtoFragil ^ produtoCaro); // OU exclusivo
   print(!produtoFragil); // Negação
