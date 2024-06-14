0c917c75040baef8ec5f199b8db54fab01a15a54
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-14 09:43:20 -0300

    variavéis

---
+++ fundamentos/variaveis_1.dart
@@ -0,0 +1,8 @@
+void main() {
+  int a = 10;
+  double b = 10.5;
+  String c = 'ola';
+  bool d = true;
+  dynamic e = 'ola';
+  print('$a, $b, $c, $d, $e');
+}
---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var a = 2;
+  var b = 3.5;
+  var texto = "O valor da soma é : ";
+
+  print(texto + (a + b).toString());
+  print(a.runtimeType);
+}
