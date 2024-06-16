b142a08707ab481b84dae3779f4ba40c3f2d04b8
Gabriel Pereira <gabriel02ps@gmail.com>
2024-06-16 12:32:31 -0300

    parâmetros dinâmicos

--- funcoes/dinamico.dart
+++ funcoes/dinamico.dart
@@ -1,5 +1,13 @@
 main() {
   juntar(1, 9);
+  juntar('ola ', 'mundo');
+
+  String resultado = juntar(1, ' ola');
+
+  print(resultado.toUpperCase());
 }
 
-juntar(a, b) {}
+String juntar(dynamic a, b) {
+  print(a.toString() + b.toString());
+  return a.toString() + b.toString();
+}
